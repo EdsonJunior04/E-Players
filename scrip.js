10ccec89a51442a35643fd75aaf97cae5d2d3598
Edson Junior <ejr_04@outlook.com>
2021-04-28 13:36:33 -0300

    e-players concluido

---
+++ scrip.js
@@ -0,0 +1,37 @@
+const burguer = document.querySelector(".burguer")
+const nav = document.querySelector(".nav_header")
+const navLinks = document.querySelectorAll('.nav_header a')
+
+
+const navSlide = () => {
+
+    //HAMBURGUER MENU
+    burguer.addEventListener("click", () => {
+        //ANIMAÇÃO HAMBURGUER
+        nav.classList.toggle("nav_ativa")
+
+        //ANIMAÇÃO LINKS
+        navLinks.forEach((link, index)=> {
+            if(link.style.animation){
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navHeaderFade 0.5s ease forwards ${index / 7 + 0.4}s`
+            }
+        })
+        // ANIMAÇÃO X DO BURGUER
+        burguer.classList.toggle('toggle')
+
+        const navSome = () => {
+            navLinks.addEventListener("click", () => {
+                nav.classList.toggleClass('nav_ativa')
+            })
+        }
+    })
+}
+
+
+
+navSlide()
+
+navSome()
